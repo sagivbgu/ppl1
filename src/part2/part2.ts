bb85bbdea777fef9d60081f6362326d7b1e1e838
sagivbgu <sagivsc@post.bgu.ac.il>
2020-04-03 16:33:12 +0300

    Part 2 Questions 1,4 + tests

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,5 +1,10 @@
+import { chain, uniq } from "ramda";
+
+const GRASS_TYPE_NAME = "Grass";
+
 /* Question 1 */
-export const partition = undefined;
+export const partition: <T>(pred: (item: T) => boolean, arr: T[]) => T[][] = (pred, arr) =>
+    [arr.filter(elem => pred(elem)), arr.filter(elem => !pred(elem))];
 
 /* Question 2 */
 export const mapMat = undefined;
@@ -31,8 +36,19 @@ interface Pokemon {
     base: Stats;
 }
 
-export const maxSpeed = undefined;
+export const maxSpeed: (pokedex: Pokemon[]) => Pokemon[] = pokedex =>
+    pokedex.reduce((acc: Pokemon[], cur: Pokemon): Pokemon[] =>
+        acc.length === 0 ? [cur] :
+            cur.base.Speed > acc[0].base.Speed ? [cur] :
+                cur.base.Speed === acc[0].base.Speed ? acc.concat(cur) :
+                    acc
+        , []);
 
-export const grassTypes = undefined;
 
-export const uniqueTypes = undefined;
+export const grassTypes: (pokedex: Pokemon[]) => string[] = pokedex =>
+    pokedex.filter(pokemon => pokemon.type.includes(GRASS_TYPE_NAME))
+        .map(pokemon => pokemon.name.english)
+        .sort();
+
+export const uniqueTypes: (pokedex: Pokemon[]) => string[] = pokedex =>
+    uniq(chain((pokemon => pokemon.type), pokedex)).sort();
--- test/part2.test.ts
+++ test/part2.test.ts
@@ -1,5 +1,88 @@
 import { expect } from "chai";
+import { partition, maxSpeed, grassTypes, uniqueTypes } from "../src/part2/part2";
+import { pokedex as fullPokedex } from "./test_pokedex";
+
+const partialPokedex = [{ "id": 15, "name": { "english": "NOAME", "japanese": "NOAMJ", "chinese": "NOAMC", "french": "NOAMF" }, "type": ["Grass", "Fire"], "base": { "HP": 97, "Attack": 99, "Defense": 95, "Sp. Attack": 67, "Sp. Defense": 87, "Speed": 105 } },
+{ "id": 17, "name": { "english": "ALONE", "japanese": "ALONJ", "chinese": "ALONC", "french": "ALONF" }, "type": ["Water", "Fire"], "base": { "HP": 97, "Attack": 99, "Defense": 95, "Sp. Attack": 67, "Sp. Defense": 87, "Speed": 56 } },
+{ "id": 16, "name": { "english": "AVIE", "japanese": "AVIJ", "chinese": "AVIC", "french": "AVIF" }, "type": [], "base": { "HP": 97, "Attack": 99, "Defense": 95, "Sp. Attack": 67, "Sp. Defense": 87, "Speed": 105 } },
+{ "id": 15, "name": { "english": "TOMERE", "japanese": "TOMERJ", "chinese": "TOMERC", "french": "TOMERF" }, "type": ["Grass"], "base": { "HP": 97, "Attack": 99, "Defense": 95, "Sp. Attack": 67, "Sp. Defense": 87, "Speed": 80 } }];
 
 describe("Assignment 1 Part 2", () => {
-    
+    it("partition extracts even numbers", () => {
+        const isEven: (n: number) => boolean = n => n % 2 === 0;
+        const wholeList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        const evens = [2, 4, 6, 8];
+        const odds = [1, 3, 5, 7, 9];
+        expect(partition(isEven, wholeList)).to.deep.equal([evens, odds]);
+    });
+
+    it("partition extracts strings with 's'", () => {
+        const containsS: (str: string) => boolean = str => str.includes("s");
+        const words = ["No no", "Yes yes", "AB", "AC", "AsDF"];
+        const withS = ["Yes yes", "AsDF"];
+        const withoutS = ["No no", "AB", "AC"];
+        expect(partition(containsS, words)).to.deep.equal([withS, withoutS]);
+    });
+
+    it("maxSpeed partial pokedex", () => {
+        expect(maxSpeed(partialPokedex)).to.deep.equal([{ "id": 15, "name": { "english": "NOAME", "japanese": "NOAMJ", "chinese": "NOAMC", "french": "NOAMF" }, "type": ["Grass", "Fire"], "base": { "HP": 97, "Attack": 99, "Defense": 95, "Sp. Attack": 67, "Sp. Defense": 87, "Speed": 105 } },
+        { "id": 16, "name": { "english": "AVIE", "japanese": "AVIJ", "chinese": "AVIC", "french": "AVIF" }, "type": [], "base": { "HP": 97, "Attack": 99, "Defense": 95, "Sp. Attack": 67, "Sp. Defense": 87, "Speed": 105 } }]);
+    });
+
+    it("maxSpeed full pokedex", () => {
+        expect(maxSpeed(fullPokedex)).to.deep.equal([
+            {
+                id: 6,
+                name: {
+                    english: 'Charizard',
+                    japanese: 'リザードン',
+                    chinese: '喷火龙',
+                    french: 'Dracaufeu'
+                },
+                type: ['Fire', 'Flying'],
+                base: {
+                    HP: 78,
+                    Attack: 84,
+                    Defense: 78,
+                    'Sp. Attack': 109,
+                    'Sp. Defense': 85,
+                    Speed: 100
+                }
+            },
+            {
+                id: 8,
+                name: {
+                    english: 'Wartortle',
+                    japanese: 'カメール',
+                    chinese: '卡咪龟',
+                    french: 'Carabaffe'
+                },
+                type: ['Water'],
+                base: {
+                    HP: 59,
+                    Attack: 63,
+                    Defense: 80,
+                    'Sp. Attack': 65,
+                    'Sp. Defense': 80,
+                    Speed: 100
+                }
+            }
+        ])
+    });
+
+    it("grassType partial pokedex", () => {
+        expect(grassTypes(partialPokedex)).to.deep.equal(["NOAME", "TOMERE"])
+    });
+
+    it("grassType full pokedex", () => {
+        expect(grassTypes(fullPokedex)).to.deep.equal(['Bulbasaur', 'Ivysaur', 'Venusaur'])
+    });
+
+    it("uniqueTypes partial pokedex", () => {
+        expect(uniqueTypes(partialPokedex)).to.deep.equal(["Fire", "Grass", "Water"])
+    });
+
+    it("uniqueTypes full pokedex", () => {
+        expect(uniqueTypes(fullPokedex)).to.deep.equal(["Bug", "Fire", "Flying", "Grass", "Normal", "Poison", "Water"]);
+    });
 });
